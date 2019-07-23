a236fda088c61b8cb241e7d934ba64585a424727
wackieju <jiajie.wu96@gmail.com>
2019-07-22 20:38:14 -0700

    Added earthquake list item and adapter

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
@@ -0,0 +1,23 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class EarthquakeListAdapter extends ArrayAdapter {
+
+    public EarthquakeListAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+
+        return super.getView(position, convertView, parent);
+    }
+}
