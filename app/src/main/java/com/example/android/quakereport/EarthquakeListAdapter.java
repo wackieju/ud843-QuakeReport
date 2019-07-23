05f2e7ee322ccea521d60ef486bb4efd7274664f
wackieju <jiajie.wu96@gmail.com>
2019-07-22 21:51:04 -0700

    updated earthquake adapter

--- app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeListAdapter.java
@@ -3,21 +3,40 @@ package com.example.android.quakereport;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class EarthquakeListAdapter extends ArrayAdapter {
+import java.util.ArrayList;
 
-    public EarthquakeListAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+public class EarthquakeListAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeListAdapter(@NonNull Context context, ArrayList<Earthquake> earthquakes) {
+        super(context, 0, earthquakes);
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, parent, false);
+        }
 
-        return super.getView(position, convertView, parent);
+        TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude_text_view);
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location_text_view);
+        TextView dateTextView = (TextView) listItemView.findViewById(R.id.date_text_view);
+
+        Earthquake earthquake = getItem(position);
+
+        if(earthquake !=null){
+            magnitudeTextView.setText(String.format(Float.toString(earthquake.getMagnitude())));
+            locationTextView.setText(earthquake.getLocation());
+            dateTextView.setText(earthquake.getDate());
+        }
+
+        return listItemView;
     }
 }
