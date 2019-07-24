0725a1669f2b7b29ae3731c229e9af6b92838b81
wackieju <jiajie.wu96@gmail.com>
2019-07-24 01:09:25 -0700

    added website intent

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,8 +15,13 @@
  */
 package com.example.android.quakereport;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -38,11 +43,22 @@ public class EarthquakeActivity extends AppCompatActivity {
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
+
         // Create a new {@link ArrayAdapter} of earthquakes
-        EarthquakeListAdapter adapter = new EarthquakeListAdapter(this, earthquakes);
+        final EarthquakeListAdapter adapter = new EarthquakeListAdapter(this, earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Earthquake earthquake = adapter.getItem(i);
+                Uri earthquakeURI = Uri.parse(earthquake.getDetailURL());
+                Intent intent = new Intent(Intent.ACTION_VIEW, earthquakeURI);
+                startActivity(intent);
+
+            }
+        });
     }
 }
