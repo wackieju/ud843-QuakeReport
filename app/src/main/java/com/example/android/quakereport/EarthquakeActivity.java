62e501b6ed9070419055e5462f21c99322abe6c6
wackieju <jiajie.wu96@gmail.com>
2019-07-25 01:24:35 -0700

    Added check for no internet

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,16 +16,18 @@
 package com.example.android.quakereport;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -39,7 +41,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     private static final int EQ_LOADER_ID = 1;
     private EarthquakeListAdapter mAdapter;
 
-    private TextView emptyListTextView;
+    private TextView mEmptyListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +50,8 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
-
-        emptyListTextView = (TextView) findViewById(R.id.no_earthquakes_text_view);
+        earthquakeListView.setEmptyView(mEmptyListTextView);
+        mEmptyListTextView = (TextView) findViewById(R.id.no_earthquakes_text_view);
 
         // Create a new {@link ArrayAdapter} of earthquakes
         mAdapter = new EarthquakeListAdapter(this, new ArrayList<Earthquake>());
@@ -57,7 +59,6 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
         earthquakeListView.setAdapter(mAdapter);
-        earthquakeListView.setEmptyView(emptyListTextView);
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -69,6 +70,25 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
             }
         });
 
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EQ_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.progressBar);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyListTextView.setText(R.string.no_internet_connection);
+        }
         getLoaderManager().initLoader(EQ_LOADER_ID, null, this);
 
     }
@@ -80,7 +100,11 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
 
     @Override
     public void onLoadFinished(Loader<List<Earthquake>> loader, List<Earthquake> earthquakes) {
-        emptyListTextView.setText(R.string.no_earthquakes);
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        mEmptyListTextView.setText(R.string.no_earthquakes);
+
         mAdapter.clear();
         if(earthquakes!=null && !earthquakes.isEmpty()){
             mAdapter.addAll(earthquakes);
